497ed0c3b7e06f8414870f5bebfdc1c34d76eb35
mirhorodskiy <mirhorodskiy@gmail.com>
2021-10-27 18:18:18 +0300

    forecast service fixed

--- src/main/java/com/kpi/lab2/model/service/ForecastService.java
+++ src/main/java/com/kpi/lab2/model/service/ForecastService.java
@@ -13,34 +13,34 @@ import org.springframework.stereotype.Service;
 @Service
 public class ForecastService {
 
-    ForecastRepository forecastRepository;
-    DateRepository dateRepository;
-    LocationRepository locationRepository;
+  ForecastRepository forecastRepository;
+  DateRepository dateRepository;
+  LocationRepository locationRepository;
 
-    @Autowired
-    public ForecastService(ForecastRepository forecastRepository,
-                           DateRepository dateRepository,
-                           LocationRepository locationRepository) {
-        this.forecastRepository = forecastRepository;
-        this.dateRepository = dateRepository;
-        this.locationRepository = locationRepository;
+  @Autowired
+  public ForecastService(ForecastRepository forecastRepository,
+                         DateRepository dateRepository,
+                         LocationRepository locationRepository) {
+    this.forecastRepository = forecastRepository;
+    this.dateRepository = dateRepository;
+    this.locationRepository = locationRepository;
+  }
+
+  public Forecast getForecast(String cityTitle, int day, String month, int year)
+          throws CityIsNotExistsException, DateIsNotExistsException, ForecastIsNotExistsException {
+    if (!locationRepository.existsByCityTitle(cityTitle)) {
+      throw new CityIsNotExistsException("Wrong city title or data is unavailable!");
     }
 
-    public Forecast getForecast(String cityTitle, int day, String month, int year)
-            throws CityIsNotExistsException, DateIsNotExistsException, ForecastIsNotExistsException {
-        if (!locationRepository.existsByCityTitle(cityTitle)) {
-            throw new CityIsNotExistsException("Wrong city title or data is unavailable!");
-        }
-
-        if (!dateRepository.existsByDayAndMonthAndYear((byte) day, month, year)) {
-            throw new DateIsNotExistsException("Wrong date or there are no data!");
-        }
-
-        Forecast forecast = forecastRepository.findByLocationAndDate(locationRepository.findByCityTitle(cityTitle).getId(),
-                dateRepository.findByDayAndMonthAndYear((byte) day, month, year).getId());
-        if (forecast == null) {
-            throw new ForecastIsNotExistsException("There are no forecast for that date!");
-        }
-        return forecast;
+    if (!dateRepository.existsByDayAndMonthAndYear((byte) day, month, year)) {
+      throw new DateIsNotExistsException("Wrong date or there are no data!");
     }
+
+    Forecast forecast = forecastRepository.findByLocationAndDate(locationRepository.findByCityTitle(cityTitle).getId(),
+            dateRepository.findByDayAndMonthAndYear((byte) day, month, year).getId());
+    if (forecast == null) {
+      throw new ForecastIsNotExistsException("There are no forecast for that date!");
+    }
+    return forecast;
+  }
 }
